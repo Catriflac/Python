ba2d93f342bbcdfe18f068d6ae8e315d40ee3683
Catriflac <115920743+Catriflac@users.noreply.github.com>
2022-10-16 17:54:07 +0200

    Update boiler.py

--- boiler.py
+++ boiler.py
@@ -9,17 +9,18 @@ print(msg + "\nTípus: " + str(type(msg)) + "\n")
 
 #Változó beolvasása és típuskonverziós kiírása
 szam = int(input("Adj meg egy számot: "))
-#szam = szam+10
 print("Hozzáadtam 10-et. Szívesen: " + str(szam+10) + "\n")
+szam = szam + 2.54123549846
+print("Hozzáadtam 2.54-et, mert bizony ilyet is tudok: %.2f\n" % (szam))
 
 #Tömb deklarálása és kiírása
 koktel = [
     "Gin",
     "Rum",
+    "Málnaszörp",
     "Kóla",
     "Narancslé",
     "Szóda",
-    "Málnaszörp",
     ]
 
 
@@ -53,17 +54,23 @@ mennyiseg.remove(" hl ") #mennyiseg.pop(4)
 #Futtatásnál ebből nem lesz probléma, mert mire ez az előre definiált alprogram hívásra kerül, már létezik a mixer()
 def pincer():   
     print("A recept a professzor híres koktéljához:")
-    recept_hossz = random.randint(1,len(koktel))
-    for i in range(recept_hossz):
+    recept_hossz = random.randint(1,len(koktel))    #A recept hossza legyen mindig változó
+    for i in range(recept_hossz):   #És annyiszor hívja meg a mixert hozzáadni egy összetevőt
         mixer()
-    print(veletlen_osszetevo[rand])
+    print(veletlen_osszetevo[rand]) #Végül egyszer adjunk hozzá egy véletlen összetevőt
 
 def mixer():
-    x = random.randint(0,len(mennyiseg)-1)
     y = random.randint(0,len(koktel)-1)
+    
+    if (koktel[y] == "Gin") or (koktel[y] == "Rum") or (koktel[y] == "Málnaszörp"):  #legyünk mértékletesek a töményekkel
+        x = random.randint(0,1)
+    else:
+        x = random.randint(0,len(mennyiseg)-1)
+
     z = random.randint(1,10)
+    
     print(str(z) + mennyiseg[x] + koktel[y])
-    koktel.pop(y)
+    koktel.pop(y)   #Az aktuális indexet töröljük is, hogy ne lehessen kétszer hívni valamit
 
 #Itt hívjuk az alprogramunkat
 #A hívás pillanatában már definiált kell, hogy legyen minden résztvevő
